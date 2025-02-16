f3a24d06263724e08c8954a27d14cecd2acfedf0
senselogic <dev.senselogic@gmail.com>
2025-02-16 20:20:05 +0100

    % install

--- TEST/install.bat
+++ TEST/install.bat
@@ -1,9 +1,12 @@
 python.exe --version
-echo requires python 3.11.7+
-echo requires CUDA 12.1
-echo "https://developer.nvidia.com/cuda-12-1-0-download-archive?target_os=Windows&target_arch=x86_64&target_version=10&target_type=exe_local
+echo requires python 3.12.9+
+echo "https://www.python.org/ftp/python/3.12.9/python-3.12.9-amd64.exe"
 python.exe -m pip install --upgrade pip
 pip uninstall -y diffusers
-pip install accelerate optimum-quanto protobuf sentencepiece torch torchvision transformers
+rem echo requires CUDA 12.1
+rem echo "https://developer.nvidia.com/cuda-12-1-0-download-archive?target_os=Windows&target_arch=x86_64&target_version=11&target_type=exe_local
+rem pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
+pip install torch torchvision
+pip install accelerate optimum-quanto protobuf sentencepiece transformers pandas
 pip install git+https://github.com/huggingface/diffusers.git
 pause
